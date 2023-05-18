40236f6370c3aaa685e02b0d0d9bf7aba867b71f
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-05-18 14:02:03 -0400

    test(api): authenticate using GoTrue API directly

--- tests/supabase/auth.spec.ts
+++ tests/supabase/auth.spec.ts
@@ -48,3 +48,31 @@ test("get user from saved session", async () => {
 
   expect(data.user?.email).toEqual(env.SUPABASE_TEST_EMAIL);
 });
+
+/**
+  * Copied from `https://app.supabase.com/project/fbzquxjugehuhoonbgwc/api?page=users`
+  *
+  * ```sh
+  * curl -X POST 'https://fbzquxjugehuhoonbgwc.supabase.co/auth/v1/token?grant_type=password' \
+  * -H "apikey: SUPABASE_KEY" \
+  * -H "Content-Type: application/json" \
+  * -d '{
+  *   "email": "someone@email.com",
+  *   "password": "rlmHmDVPsUKdXqGrAynB"
+  * }'
+  * ```
+  */
+test("authenticate using GoTrue API directly", async ({ request }) => {
+  const url = new URL("/auth/v1/token?grant_type=password", env.NEXT_PUBLIC_SUPABASE_URL);
+  const res = await request.post(url.toString(), {
+    headers: {
+      apikey: env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    },
+    data: {
+      email: env.SUPABASE_TEST_EMAIL,
+      password: env.SUPABASE_TEST_PASSWORD,
+    },
+  });
+  expect(res).toBeOK();
+  expect(await res.json()).toHaveProperty("access_token");
+})
