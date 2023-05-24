4497fa7e01f636b02ce4267768bd6333f8427929
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-05-23 20:29:46 -0400

    test(api): bundle supabase browser lib
    Bundling the `@supabase/auth-helpers-shared` library to use in the
    browser context allows us to initialize and save the authorization
    cookies for subsequent tests.
    
    Since our application will be using the auth helpers on both the server
    and client, the auth token and refresh token will be sent over in the
    "supabase-auth-token" cookie.

--- tests/api/profile.spec.ts
+++ tests/api/profile.spec.ts
@@ -1,7 +1,9 @@
 import { BrowserContext, expect, test } from '@playwright/test';
+import * as esbuild from "esbuild";
 import { env } from "@/env.mjs";
 
 const STORAGE_STATE = "./playwright/.auth/storageState.json";
+const SUPABASE_SCRIPT = "./playwright/out/supabase-auth-helpers.js";
 let ACCESS_TOKEN: string;
 
 test.beforeAll(async ({ browser }) => {
@@ -17,9 +19,19 @@ test.beforeAll(async ({ browser }) => {
   const page = await ctx.newPage();
   await page.goto("/");
 
-  // TODO: use the @supabase/auth-helpers-shared library
   // Add the Supabase library to the page
-  await page.addScriptTag({ path: "./node_modules/@supabase/supabase-js/dist/umd/supabase.js" });
+  await esbuild.build({
+    entryPoints: [
+      "./node_modules/@supabase/auth-helpers-shared/dist/index.mjs",
+    ],
+    bundle: true,
+    minify: true,
+    globalName: "supabase_auth_helpers",
+    sourcemap: true,
+    outfile: SUPABASE_SCRIPT,
+  });
+
+  await page.addScriptTag({ path: SUPABASE_SCRIPT });
 
   // Start waiting for a response before we add our script
   const getTokenResponse = page.waitForResponse(res => res.url().startsWith(env.NEXT_PUBLIC_SUPABASE_URL));
@@ -29,17 +41,12 @@ test.beforeAll(async ({ browser }) => {
     expect(() => { throw e }).not.toThrowError();
   });
 
+  // TODO: try using page.evaluate() to create the browser client and signin
   await page.addScriptTag({ type: "module", content: `
-    // The supabase global is added from the previous script tag
-    const client = supabase.createClient(
-      "${env.NEXT_PUBLIC_SUPABASE_URL}",
-      "${env.NEXT_PUBLIC_SUPABASE_ANON_KEY}", {
-        auth: {
-          persistsession: true,
-          autoRefreshToken: true,
-        },
-      },
-    );
+    const client = supabase_auth_helpers.createBrowserSupabaseClient({
+      supabaseUrl: "${env.NEXT_PUBLIC_SUPABASE_URL}",
+      supabaseKey: "${env.NEXT_PUBLIC_SUPABASE_ANON_KEY}",
+    });
 
     // Sign in with password to get an access token
     const { data, error } = await client.auth.signInWithPassword({
@@ -86,15 +93,10 @@ test.describe("/api/profile/", async () => {
   test("GET", async ({ request }) => {
     const url = new URL("/api/profile", env.NEXT_PUBLIC_SITE_ORIGIN);
 
-    const res = await request.get(url.toString(), {
-      headers: {
-        apikey: env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-        Authorization: `Bearer ${ACCESS_TOKEN}`
-      },
-    });
+    const res = await request.get(url.toString());
 
-    expect(res).toBeOK();
     const data = await res.json();
-    expect(data).toBeDefined();
+    expect(res).toBeOK();
+    expect(data).toHaveProperty("username");
   });
 });
