cde4a270ae957d5c6a6f0ca439363c5ba41527eb
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-05-30 15:25:24 -0400

    refactor(test): reorder and use soft assertions

--- tests/api/recommendation.spec.ts
+++ tests/api/recommendation.spec.ts
@@ -1,4 +1,4 @@
-import { BrowserContext, expect, test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 import { env } from "@/env.mjs";
 
 test.describe("/api/recommendation", async () => {
@@ -7,16 +7,14 @@ test.describe("/api/recommendation", async () => {
     const url = new URL("/api/recommendation", env.NEXT_PUBLIC_SITE_ORIGIN);
 
     const res = await request.get(url.toString());
+    expect.soft(res).toBeOK();
 
-    const status = res.status()
-    const data = await res.json();
-
-    if (status === 429) {
+    if (res.status() === 429) {
       console.log("Your token usage might be expired");
       return;
     }
 
-    console.log(JSON.stringify(data, null, 2));
-    expect(res).toBeOK();
+    const data = await res.json();
+    console.log(data);
   });
 });
