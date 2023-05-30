2d1fc8cfa7dd26d18d15af37a9e3df0cf5c920b5
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-05-30 19:26:40 -0400

    fix(api): return correct status

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -14,9 +14,11 @@ export async function GET() {
   // Gets session information from the "supabase-auth-token" cookie
   const { data: { session }, error: authError } = await supabase.auth.getSession();
 
-  if (!session || authError) return NextResponse.json(authError, {
-    status: 500,
-  });
+  if (authError) {
+    return NextResponse.json(authError.message, { status: authError.status });
+  } else if (!session) {
+    return NextResponse.json("User is not authenticated", { status: 401, });
+  }
 
   const { data: profiles, status, statusText } = await supabase
     .from("profiles")
