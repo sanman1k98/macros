cc9b04b9e2752378469b7fd42cfab60ea816d6c1
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-05-30 17:27:58 -0400

    refactor(test): implement test
    Specify query params that search for Chick-fil-a and expect the
    response to return more than one result in the array.

--- tests/api/nutritionix.spec.ts
+++ tests/api/nutritionix.spec.ts
@@ -1,15 +1,25 @@
 import { expect, test } from '@playwright/test';
 import { env } from "@/env.mjs";
+import { URLSearchParams } from 'url';
 
 test.describe("/api/nutritionix/search", async () => {
 
-  test.skip("GET", async ({ request }) => {
+  test("GET", async ({ request }) => {
     const url = new URL("/api/nutritionix/search", env.NEXT_PUBLIC_SITE_ORIGIN);
 
-    const res = await request.get(url.toString());
+    const params = new URLSearchParams([
+      ["query",        "chick fil a"],  // search for Chick-fil-a
+      ["branded",      "true"],         // include branded foods
+      ["branded_type", "1"],            // 1 = restuarant
+      ["common",       "false"],        // don't include common foods
+    ]);
+
+    const endpoint = `${url.origin}${url.pathname}?${params.toString()}`;
+    const res = await request.get(endpoint);
     expect.soft(res).toBeOK();
 
     const data = await res.json();
-    console.log(data);
+    expect(data).toHaveProperty("branded");
+    expect(data.branded.length).toBeGreaterThan(1);
   });
 });
