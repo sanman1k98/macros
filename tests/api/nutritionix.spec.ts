4eed55a86c16009b7763e2123a57d1d019f2f836
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-05-31 19:38:33 -0400

    test(api): manually filter response data

--- tests/api/nutritionix.spec.ts
+++ tests/api/nutritionix.spec.ts
@@ -65,4 +65,41 @@ test.describe("GET", async () => {
       console.log(item.food_name);
     });
   });
+
+  test("get a list of foods from Chick-Fil-A and filter by calories", async ({ request }) => {
+    const url = new URL("/api/nutritionix/search", env.NEXT_PUBLIC_SITE_ORIGIN);
+
+    const params = new URLSearchParams([
+      ["query",        "chick fil a"], // search for Chick-fil-a
+      ["branded",      "true"],        // include branded foods
+      ["branded_type", "1"],           // 1 = restuarant
+      ["common",       "false"],       // don't include common foods
+      ["detailed",     "true"],        // include detailed nutrient fields
+    ]);
+
+    const endpoint = `${url.origin}${url.pathname}?${params.toString()}`;
+    const res = await request.get(endpoint);
+    expect.soft(res).toBeOK();
+
+    const data = await res.json();
+    expect(data).toHaveProperty("branded");
+
+    const { branded } = data;
+    expect.soft(branded.length).toBeGreaterThan(1);
+
+    const filtered = branded.filter((item: any) => {
+      expect.soft(item).toHaveProperty("food_name");
+      expect.soft(item).toHaveProperty("brand_name");
+      expect.soft(item).toHaveProperty("nix_brand_id");
+      expect.soft(item).toHaveProperty("nix_item_id");
+      expect.soft(item).toHaveProperty("nf_calories");
+      // only get items that are less than 100 calories
+      return item.nf_calories <= 100;
+    });
+
+    filtered.map((item: any) => {
+      console.log(item.food_name, item.nf_calories);
+      console.log(item);
+    })
+  });
 });
