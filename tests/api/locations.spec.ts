00ce73da34aee91a9d3f42b78400d381a1d494c6
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-05-31 18:45:48 -0400

    teat(api): test locations in Mechanicsburg

--- tests/api/locations.spec.ts
+++ tests/api/locations.spec.ts
@@ -29,4 +29,31 @@ test.describe("GET", async () => {
       expect.soft(loc).toHaveProperty("distance_km");
     });
   });
+
+  test("get restaurants in Mechanicsburg", async ({ request }) => {
+    const url = new URL("/api/locations", env.NEXT_PUBLIC_SITE_ORIGIN);
+
+    const params = new URLSearchParams([
+      ["ll",       "40.21404,-77.01017"], // Mechanicsburg, PA
+      ["distance", "10mi"],               // search radius of 10 miles
+    ]);
+
+    const endpoint = `${url.origin}${url.pathname}?${params.toString()}`;
+    const res = await request.get(endpoint);
+    expect.soft(res).toBeOK();
+
+    const data = await res.json();
+    expect(data).toHaveProperty("locations");
+
+    const { locations } = data;
+    console.log(`Returned ${locations.length} results`)
+
+    locations.map((loc: any) => {
+      console.log(loc);
+      expect.soft(loc).toHaveProperty("name");
+      expect.soft(loc).toHaveProperty("brand_id");
+      expect.soft(loc).toHaveProperty("website");
+      expect.soft(loc).toHaveProperty("distance_km");
+    });
+  });
 });
