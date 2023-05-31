5e24594b3ba04fd08cfd968929939a5ce89e7ea8
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-05-31 12:38:39 -0400

    feat(api): add route to get restaurant locations
    Write test to get restaurant locations around Philly.

---
+++ src/app/api/locations/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+import { env } from "@/env.mjs";
+
+// Do not cache this route.
+export const revalidate = false;
+
+export async function GET(req: NextRequest) {
+  const url = new URL("/v2/locations", "https://trackapi.nutritionix.com");
+
+  // includes "?" followed by params
+  const search = req.nextUrl.search;
+
+  const res = await fetch(`${url.href}${search}`, {
+    method: "GET",
+    headers: {
+      "x-app-id": env.NUTRITIONIX_APP_ID,
+      "x-app-key": env.NUTRITIONIX_APP_KEY,
+    },
+  });
+
+  const data = await res.json();
+
+  return NextResponse.json(data);
+}
---
+++ tests/api/locations.spec.ts
@@ -0,0 +1,33 @@
+import { expect, test } from '@playwright/test';
+import { env } from "@/env.mjs";
+import { URLSearchParams } from 'url';
+
+test.describe("GET", async () => {
+
+  test("get restaurants around Philly", async ({ request }) => {
+    const url = new URL("/api/locations", env.NEXT_PUBLIC_SITE_ORIGIN);
+
+    const params = new URLSearchParams([
+      ["ll",       "39.9526,-75.1651"], // Philadelphia, PA
+      ["distance", "30mi"],             // search radius of 30 miles
+    ]);
+
+    const endpoint = `${url.origin}${url.pathname}?${params.toString()}`;
+    const res = await request.get(endpoint);
+    expect.soft(res).toBeOK();
+
+    const data = await res.json();
+    expect(data).toHaveProperty("locations");
+
+    const { locations } = data;
+    console.log(`Returned ${locations.length} results`)
+
+    locations.map((loc: any) => {
+      console.log(loc);
+      expect.soft(loc).toHaveProperty("name");
+      expect.soft(loc).toHaveProperty("brand_id");
+      expect.soft(loc).toHaveProperty("website");
+      expect.soft(loc).toHaveProperty("distance_km");
+    });
+  });
+});
