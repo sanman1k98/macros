adf7aa24d16439c56168ae8173d7ebad205fb121
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-05-31 18:50:10 -0400

    refactor(api): handle POST request for search

--- src/app/api/nutritionix/search/route.ts
+++ src/app/api/nutritionix/search/route.ts
@@ -24,3 +24,25 @@ export async function GET(req: NextRequest) {
     status: res.status,
   });
 }
+
+export async function POST(req: NextRequest) {
+  const url = new URL("/v2/search/instant", "https://trackapi.nutritionix.com");
+
+  const json = await req.text();
+  console.log(json);
+
+  const res = await fetch(url.href, {
+    method: "POST",
+    headers: {
+      "x-app-id": env.NUTRITIONIX_APP_ID,
+      "x-app-key": env.NUTRITIONIX_APP_KEY,
+    },
+    body: json,
+  });
+
+  const data = await res.json();
+
+  return NextResponse.json(data, {
+    status: res.status,
+  });
+}
