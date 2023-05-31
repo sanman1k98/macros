acd72399d216d20350f2ea2836ad3679350e593a
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-05-31 12:05:35 -0400

    refactor(api): cache "hello" route

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,8 +1,5 @@
 import { NextResponse } from "next/server";
 
-// Do not cache this route.
-export const revalidate = false;
-
 export async function GET() {
   return NextResponse.json("hello", {
     status: 200,
