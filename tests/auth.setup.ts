0b7ed95e621dcab76d8f1d38b646e02d18cbf810
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-06-02 20:42:35 -0400

    test: refactor `auth.setup.ts`
    Add logic to check if the storage state exists, and if the Supabase
    browser helper bundle is already built.
    
    Logs information to the console during the setup process.

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,30 +1,58 @@
 import { chromium, expect, FullConfig } from "@playwright/test";
 import * as esbuild from "esbuild";
+import * as fs from "fs/promises";
+import { existsSync } from "fs";
 import { env } from "@/env.mjs";
 
 const CLIENT_SCRIPT = "./playwright/out/supabase-auth-helpers.js";
 
-export default async function authSetup(config: FullConfig) {
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
+/**
+ * Log info the the terminal
+ */
+const info = (str: string) => console.info(`=> ${str}...`);
 
+export default async function authSetup(config: FullConfig) {
   // Destructure config
   const { storageState, baseURL } = config.projects[0].use;
+
+  let supabaseCookie;
+
+  info("Checking for a saved Supabase auth cookie");
+
+  try {
+    const contents = await fs.readFile(storageState as string, { encoding: "utf-8" });
+    const store = JSON.parse(contents);
+    supabaseCookie = store.cookies.some((cookie: any) => cookie.name.includes("supabase"));
+  } catch (e) {
+    console.log(e);
+    info("Did not find saved Supabase cookie");
+  }
+
+  if (supabaseCookie) {
+    return info("Using saved Supabase auth cookie");
+  }
+
+  info("Opening browser")
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
 
   // Navigate to the home page
   await page.goto(baseURL!);
 
-  // Bundle Supabase client libraries
-  await esbuild.build({
-    entryPoints: [
-      "./node_modules/@supabase/auth-helpers-shared/dist/index.mjs",
-    ],
-    bundle: true,
-    minify: true,
-    globalName: "supabase_auth_helpers",
-    sourcemap: true,
-    outfile: CLIENT_SCRIPT,
-  });
+  if (!existsSync(CLIENT_SCRIPT)) {
+    info("Building Supabase Auth Helpers...")
+    await esbuild.build({
+      entryPoints: [
+        "./node_modules/@supabase/auth-helpers-shared/dist/index.mjs",
+      ],
+      bundle: true,
+      minify: true,
+      outfile: CLIENT_SCRIPT,
+      sourcemap: true,
+      // When the script runs it creates a global variable
+      globalName: "supabase_auth_helpers",
+    });
+  }
 
   // Start waiting for a response before we add our script
   const getSupabaseResponse = page.waitForResponse(res => res.url().startsWith(env.NEXT_PUBLIC_SUPABASE_URL));
@@ -54,10 +82,11 @@ export default async function authSetup(config: FullConfig) {
     if (!data.session && error) throw error;
   `});
 
+  // Resolve the promise
   await getSupabaseResponse;
 
   // Write the storage state to a file
-  const store = await page.context().storageState({ path: storageState as string });
+  await page.context().storageState({ path: storageState as string });
 
   // Ensure context closes gracefully and any artifacts are fully flushed and saved
   await browser.close();
