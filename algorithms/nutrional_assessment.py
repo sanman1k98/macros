04942516d5cd2a75edfe28d8f1031052a534a952
Raja Giddi <97639175+RajaGiddi@users.noreply.github.com>
2023-06-06 23:23:44 -0400

    added code to account for common foods and show proof of concept about allocated calorie reduction from food intake

--- algorithms/nutrional_assessment.py
+++ algorithms/nutrional_assessment.py
@@ -1,4 +1,7 @@
 import math
+import random
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def calculate_bmi(weight, height):
     # Convert height from cm to meters
@@ -107,3 +110,67 @@ else:
 print("BMI:", round(bmi, 2))
 print("BMR:", round(bmr))
 print("TEE:", round(tee))
+
+# Food Example Implementation for MVP V0.1:
+
+import random
+
+calories_dict = {
+    "Apple": 59,
+    "Banana": 151,
+    "Grapes": 100,
+    "Orange": 53,
+    "Pear": 82,
+    "Peach": 67,
+    "Pineapple": 82,
+    "Strawberry": 53,
+    "Watermelon": 50,
+    "Asparagus": 27,
+    "Broccoli": 45,
+    "Carrots": 50,
+    "Cucumber": 17,
+    "Eggplant": 35,
+    "Lettuce": 5,
+    "Tomato": 22,
+    "Beef, regular, cooked": 142,
+    "Chicken, cooked": 136,
+    "Tofu": 86,
+    "Egg": 78,
+    "Fish, Catfish, cooked": 136,
+    "Pork, cooked": 137,
+    "Shrimp, cooked": 56,
+    "Bread, white": 75,
+    "Butter": 102,
+    "Caesar salad": 481,
+    "Cheeseburger": 285,
+    "Hamburger": 250,
+    "Dark Chocolate": 155,
+    "Corn": 132,
+    "Pizza": 285,
+    "Potato": 130,
+    "Rice": 206,
+    "Sandwich": 200,
+    "Beer": 154,
+    "Coca-Cola Classic": 150,
+    "Diet Coke": 0,
+    "Milk (1%)": 102,
+    "Milk (2%)": 122,
+    "Milk (Whole)": 146,
+    "Orange Juice": 111,
+    "Apple cider": 117,
+    "Yogurt (low-fat)": 154,
+    "Yogurt (non-fat)": 110
+}
+
+
+# Select a random food item
+food_item = random.choice(list(calories_dict.keys()))
+calories = calories_dict[food_item]
+
+# Print the random food item and its calories
+print(f"Random Food Item: {food_item}")
+print(f"Calories: {calories}")
+
+# Calculate the calories left after eating the random food item
+calories_left = calories_needed - calories
+print(f"Calories Left: {round(calories_left)}")
