f5484d3bc8ca01ed6df7a7708e45dda99895fc5d
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-06-29 17:58:37 -0400

    refactor: nutritionix library

--- src/lib/nutritionix/index.ts
+++ src/lib/nutritionix/index.ts
@@ -1,52 +1,53 @@
-import {  } from "next/cache";
-const NUTRITIONIX_URL = "https://trackapi.nutritionix.com";
+import { env } from "@/env.mjs";
 
 type Restaurant = { name: string, id: string };
 
-export default class NutritionixClient {
+export const NUTRITIONIX_URL = "https://trackapi.nutritionix.com";
 
-  protected url = NUTRITIONIX_URL;
-  protected fetch: typeof fetch;
-  protected headers: {
-    [key: string]: string,
-    "x-app-id": string,
-    "x-app-key": string,
-  }
+export const NUTRITIONIX_HEADERS = {
+  "x-app-id": env.NUTRITIONIX_APP_ID,
+  "x-app-key": env.NUTRITIONIX_APP_KEY,
+};
 
-  public restaurants: Restaurant[];
+export const restaurants = new Map<string, string>();
 
-  private constructor( protected nutritionixAppId: string, protected nutritionixAppKey: string) {
-    if (!nutritionixAppId || !nutritionixAppKey) throw new Error("missing parameter(s)");
+export async function init() {
+  const res = await fetch(
+    "https://d1gvlspmcma3iu.cloudfront.net/restaurants-3d-party.json.gz"
+  );
 
-    this.headers = {
-      "x-app-id": this.nutritionixAppId,
-      "x-app-key": this.nutritionixAppKey,
-    };
+  const data = await res.json() as Restaurant[];
 
-    this.fetch = async (input, init?) => {
-      return fetch(input, { ...init, ...this.headers });
-    }
-  }
+  for (const restaurant of data) restaurants.set(restaurant.id, restaurant.name);
 
-  static async create(
-    nutritionixAppId: string,
-    nutritionixAppKey: string
-  ): Promise<NutritionixClient> {
-    const client = new NutritionixClient(nutritionixAppId, nutritionixAppKey);
-    const res = await fetch("https://d1gvlspmcma3iu.cloudfront.net/restaurants-3d-party.json.gz");
-    client.restaurants = await res.json() as Restaurant[];
-    return client;
-  }
+  return restaurants
+}
 
-  async refreshRestaurants() {
-    const res = await fetch("https://d1gvlspmcma3iu.cloudfront.net/restaurants-3d-party.json.gz", {
-      next: { revalidate: 84_600 }
-    });
-  }
+export function getRestaurantName(id: string) {
+  return restaurants.get(id);
+} 
 
-  async getItems(id: string) {
-    this.restaurants ??= await this.refreshRestaurants();
-    const restaurant = this.restaurants.find(r => r.id === id);
-    if (!restaurant) throw new Error("invalid restaurant id");
-  }
+/**
+ * Gets the food menu items for a restaurant given its ID
+ */
+export async function getRestaurantItems(id: string) {
+  const name = restaurants.get(id);
+
+  if (!name) throw new Error("Invalid restaurant ID");
+
+  const url = new URL("/v2/search/instant", "https://trackapi.nutritionix.com");
+  const searchParams = new URLSearchParams();
+
+  searchParams.set("query", name);
+  searchParams.set("self", String(false));       // don't include user's food log
+  searchParams.set("common", String(false));     // don't include common food items
+  searchParams.set("branded", String(true));     // included branded food items
+  searchParams.set("branded_type", String(1));   // 1 = restaurant
+  searchParams.set("branded_region", String(1)); // 1 = US
+
+  const res = await fetch(`${url.href}?${searchParams.toString()}`, {
+    headers: NUTRITIONIX_HEADERS,
+  });
+
+  return await res.json();
 }
