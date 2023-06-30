d5b5d8d143fd07936b78874b7ce4fd615b7fddd1
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-06-29 22:18:46 -0400

    test: triple timeout length

--- tests/lib/openai.spec.ts
+++ tests/lib/openai.spec.ts
@@ -3,9 +3,10 @@ import { expect, test } from '@playwright/test';
 import * as openai from '@/lib/openai';
 
 test("recommend items for Chick-fil-A", async () => {
+  test.slow();
   try {
     const res = await openai.runRestaurantRecommendation("513fbc1283aa2dc80c000025");
-    console.log(res);
+    console.log(res.data.choices[0]);
   } catch (e) {
     console.log(e);
   }
